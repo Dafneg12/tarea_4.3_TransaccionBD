92284d017e9566d62b4632cbc33c5fd6440ac216
Dafneg12 <162768008+Dafneg12@users.noreply.github.com>
2025-11-04 23:05:41 -0600

    Update Productos.sql
    Productos para la revision

--- Base de datos/Productos.sql
+++ Base de datos/Productos.sql
@@ -1,5 +1,6 @@
 create database products;
 use products;
+
 create table product(
 idproducto int auto_increment primary key,
 nombre varchar(30) not null,
@@ -11,12 +12,16 @@ estado ENUM('Activo', 'Inactivo') DEFAULT 'Activo',
 fecha_actualizacion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO product (nombre, codigo, descripcion, stock, precio, estado)
-VALUES 
-('Camiseta deportiva', 'ABC1234567890', 'Camiseta de algodón color azul', 50, 249.990, 'Activo');
 
 insert into product (nombre, codigo, descripcion, stock, precio, estado) values 
-('Papas', '126048611128', 'Papas fritas', 21, 14.12, 'Activo');
+('Paleta', '812999014384', 'Pleta chipileta', 2, 5.50, 'Activo'),
+('Cerillos', '75057992', 'Cerillos flama', 14, 10.40, 'Activo'),
+('Rimel', '7501734430291', 'Rimel prosa', 21, 35.65, 'Activo'),
+('Paracetamol', '7501159590359', 'Pastillas', 12, 20.0, 'Activo'),
+('Pintura acrilica', '7501230030117', 'Pintura blanca', 5, 24.5, 1),
+('Crema', '8004995633542', 'Body lotion', 18, 212.1, 1),
+('Antitranspirante', '75062897', 'Bamboo y aloe vera', 19, 76.8, 1);
 
 select * from product;
+TRUNCATE TABLE product;
 
